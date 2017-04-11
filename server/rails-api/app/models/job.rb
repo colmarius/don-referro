e964633cb081697cd3ecd6ae8c82ff5ebba680bc
Lescenco Andrei <lescee@gmail.com>
2017-04-11 11:40:45 +0300

    add job update endpoint

--- server/rails-api/app/models/job.rb
+++ server/rails-api/app/models/job.rb
@@ -1,2 +1,3 @@
 class Job < ApplicationRecord
+  UPDATE_FIELDS = [:title, :description, :job_type, :discipline, :career_level, :language, :skills, :min_salary, :max_salary, :currency]
 end
--- server/rails-api/config/routes.rb
+++ server/rails-api/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :jobs, only: [:index, :destroy]
+    resources :jobs, only: [:index, :update, :destroy]
   end
 end
