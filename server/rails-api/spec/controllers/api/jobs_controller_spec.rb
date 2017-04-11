5920828866416e8c52efae79c563255bbbff651a
Lescenco Andrei <lescee@gmail.com>
2017-04-11 09:45:11 +0300

    added new jobs test

--- server/rails-api/spec/controllers/api/jobs_controller_spec.rb
+++ server/rails-api/spec/controllers/api/jobs_controller_spec.rb
@@ -4,11 +4,34 @@ require 'rails_helper'
 RSpec.describe Api::JobsController, type: :controller do
   let(:parsed_response) { JSON.parse(response.body) }
 
+  before do
+    Job.delete_all
+  end
+
   describe 'GET #index' do
     context 'no jobs present' do
       it 'should return empty results' do
         get :index
         expect(parsed_response).to eq []
+      end
+    end
+
+    context 'one job present' do
+      before do
+        @job = Job.create({ title: 'Junior Software Engineer',
+          description: 'test',
+          job_type: 'full_time',
+          discipline: 'hr',
+          career_level: 'entry',
+          skills: 'ruby,javascript',
+          min_salary: 5000,
+          max_salary: 8000
+        })
+      end
+
+      it 'should return an array with 1 element' do
+        get :index
+        expect(response.body).to eq [@job].to_json
       end
     end
   end
